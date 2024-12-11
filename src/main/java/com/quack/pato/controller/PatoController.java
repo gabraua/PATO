de7ab530c5a7a04cec2bc6e7173c17bb71b2ed29
gabraua <gabrielcmarinho@hotmail.com>
2024-12-11 10:29:34 -0300

    adicionado o html

--- src/main/java/com/quack/pato/controller/PatoController.java
+++ src/main/java/com/quack/pato/controller/PatoController.java
@@ -1,10 +1,28 @@
 package com.quack.pato.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/pato")
+import com.quack.pato.service.ConsumoApi;
+
+@Controller
+@RequestMapping("/")
 public class PatoController {
 
+    private final String URL_BASE = "https://random-d.uk/api/quack";
+    private ConsumoApi consumo = new ConsumoApi();
+
+    @GetMapping
+    public String home(Model model) {
+        return "patoHome";
+    }
+
+    @GetMapping("/pato")
+    public String getPato(Model model) {
+        var json = consumo.obterDados(URL_BASE);
+        System.out.println("Dados recebidos da API: " + json);
+        return "patoHome";
+    }
 }
